3e3d076aff6d22095d2c6c8ca909e6dba59fe5b4
ragor114 <69634980+ragor114@users.noreply.github.com>
2020-08-13 16:30:19 +0200

    TestKommentar

--- app/src/main/java/ur/mi/liebestagebuch/MainActivity.java
+++ app/src/main/java/ur/mi/liebestagebuch/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Vorgefertigter Code:
+        //Kommentat
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
