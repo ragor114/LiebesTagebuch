2ef39412ac9de397bef714af32f65fe5a491e9f3
BrokeMo <69637383+BrokeMo@users.noreply.github.com>
2020-08-13 16:44:30 +0200

    doofer jannik

--- app/src/main/java/ur/mi/liebestagebuch/MainActivity.java
+++ app/src/main/java/ur/mi/liebestagebuch/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         //Vorgefertigter Code:
         //Kommentat
+        // halt die klappe jannikkk
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
