e5ea09caf13d9bd5662f45fbeecc719f1ce8dde6
ragor114 <69634980+ragor114@users.noreply.github.com>
2020-08-13 15:34:30 +0200

    Added Comment

--- app/src/main/java/ur/mi/liebestagebuch/MainActivity.java
+++ app/src/main/java/ur/mi/liebestagebuch/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Vorgefertigter Code:
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
