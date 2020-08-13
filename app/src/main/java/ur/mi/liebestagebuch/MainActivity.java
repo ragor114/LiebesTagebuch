ea2b24698414ed24a0ff08e4187f912422218226
ragor114 <69634980+ragor114@users.noreply.github.com>
2020-08-13 16:33:53 +0200

    mein erster Kommentar

--- app/src/main/java/ur/mi/liebestagebuch/MainActivity.java
+++ app/src/main/java/ur/mi/liebestagebuch/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Vorgefertigter Code:
-        //Kommentat
+        //Janniks 1. Branch
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
