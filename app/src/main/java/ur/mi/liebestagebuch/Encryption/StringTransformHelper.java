a9b36a95a807d377de4d8ad8279424d36668cca7
ragor114 <69634980+ragor114@users.noreply.github.com>
2020-08-14 18:44:49 +0200

    Kommentare und getStringFromBox() ergänzt

--- app/src/main/java/ur/mi/liebestagebuch/Encryption/StringTransformHelper.java
+++ app/src/main/java/ur/mi/liebestagebuch/Encryption/StringTransformHelper.java
@@ -3,6 +3,7 @@ package ur.mi.liebestagebuch.Encryption;
 import java.util.ArrayList;
 
 import ur.mi.liebestagebuch.Boxes.Box;
+import ur.mi.liebestagebuch.Boxes.Type;
 
 public class StringTransformHelper {
     /*
@@ -13,8 +14,14 @@ public class StringTransformHelper {
     * Entwickelt von Jannik Wiese
      */
 
-    // Wandelt eine ArrayList von Boxen in einen String um.
-    // Wenn Verschlüsselung deaktiviert ist kann die Rückgabe dieser Methode in die Datenbank eingefügt werden.
+
+    /*
+     * Wandelt eine ArrayList von Boxen in einen String um.
+     * Wenn Verschlüsselung deaktiviert ist kann die Rückgabe dieser Methode in die Datenbank eingefügt werden.
+     * Eine neue Box beginnt immer mit |< und | trennt Typ von Inhalt .
+     * @param Liste von Boxen die umgewandelt werden soll
+     * @return Stringumwandlung der übergebenen Boxliste
+    */
     public static String getStringFromBoxList (ArrayList<Box> boxes){
         String boxListString = "";
 
@@ -25,7 +32,28 @@ public class StringTransformHelper {
         return boxListString;
     }
 
+    /*
+    * Wandelt eine Box in die Form "|<Typ | Stringinhalt" um.
+    * Neue Boxtypen müssen ergänzt werden, hier Verbesserung möglich.
+    * Ist die Box von einem unbekannten Typ wird sie als Text gespeichert.
+    * @param um zu wandelnde Box
+    * @return Stringumwandlung der Box in der Form |<Type | inhalt
+     */
     private static String getStringFromBox(Box box){
+        String boxString = "|<";
 
+        Type boxType = box.getType();
+        switch (boxType){
+            case PICTURE:
+                boxString += "Picture";
+                break;
+            default:
+                boxString += "Text";
+                break;
+        }
+        boxString += " | ";
+        boxString += box.getString();
+
+        return boxString;
     }
 }
