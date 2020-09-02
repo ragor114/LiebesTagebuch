4c009d98ea86afea6dd014314a5b18e0bef06586
ragor114 <69634980+ragor114@users.noreply.github.com>
2020-09-02 17:37:16 +0200

    Test-Code entfernt

--- app/src/main/java/ur/mi/liebestagebuch/MainActivity.java
+++ app/src/main/java/ur/mi/liebestagebuch/MainActivity.java
@@ -14,7 +14,7 @@ import ur.mi.liebestagebuch.Encryption.EncryptionConfig;
 import ur.mi.liebestagebuch.Encryption.StringTransformHelper;
 import ur.mi.liebestagebuch.Encryption.TestConfig;
 
-public class MainActivity extends AppCompatActivity implements CryptoListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,39 +22,6 @@ public class MainActivity extends AppCompatActivity implements CryptoListener {
         //Janniks 1. Branch
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //StringTransformHelper Test Code:
-        ArrayList<Box> testBoxList = StringTransformHelper.getBoxListFromString(TestConfig.TEST_BOXLIST_STRING);
-        String testBoxListString = StringTransformHelper.getStringFromBoxList(testBoxList);
-        Log.d("StringTransformHelper", testBoxListString);
-
-        //Verschlüsselungs Test-Code:
-        Log.d("Encryption", "Start Encryption: " + TestConfig.TEST_STRING_TO_EN_AND_DE_CRYPT);
-        StringTransformHelper.startEncryption(TestConfig.TEST_STRING_TO_EN_AND_DE_CRYPT, this);
     }
 
-
-    //Test-Implementierungen der Crypto-Listener-Methoden:
-    @Override
-    public void onEncryptionFinished(String result, byte[] iv, byte[] salt) {
-        Log.d(EncryptionConfig.LOG_TAG, "Encrypted: " + result);
-        StringTransformHelper.startDecryption(result, this, iv, salt);
-    }
-
-    @Override
-    public void onDecryptionFinished(String result) {
-        Log.d(EncryptionConfig.LOG_TAG, "Decrypted: " + result);
-    }
-
-    @Override
-    public void onEncryptionFailed() {
-        Toast toast = Toast.makeText(getApplicationContext(), R.string.encryption_failed, Toast.LENGTH_LONG);
-        toast.show();
-    }
-
-    @Override
-    public void onDecryptionFailed() {
-        Toast toast = Toast.makeText(getApplicationContext(), R.string.decryption_failed, Toast.LENGTH_LONG);
-        toast.show();
-    }
 }
