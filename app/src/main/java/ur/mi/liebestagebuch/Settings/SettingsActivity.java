8cb81085bcc9ec32f7da2494125db126022fdf10
jnsds <39292850+jnsds@users.noreply.github.com>
2020-09-08 12:02:18 +0200

    Settings XML hinzugefügt

---
+++ app/src/main/java/ur/mi/liebestagebuch/Settings/SettingsActivity.java
@@ -0,0 +1,16 @@
+package ur.mi.liebestagebuch.Settings;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ur.mi.liebestagebuch.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_layout);
+
+    }
+}
