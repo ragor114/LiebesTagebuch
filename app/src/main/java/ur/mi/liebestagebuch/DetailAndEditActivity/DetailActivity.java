be7216466783d0ca4d39a8418bf3035e40122837
ragor114 <69634980+ragor114@users.noreply.github.com>
2020-09-09 17:20:22 +0200

    added setcontentview in Detailactivity

--- app/src/main/java/ur/mi/liebestagebuch/DetailAndEditActivity/DetailActivity.java
+++ app/src/main/java/ur/mi/liebestagebuch/DetailAndEditActivity/DetailActivity.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import ur.mi.liebestagebuch.R;
+
 public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_activity);
     }
 
 }
