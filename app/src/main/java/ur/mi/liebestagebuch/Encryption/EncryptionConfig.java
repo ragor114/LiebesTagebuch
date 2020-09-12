45df195d9648e8d560c898ffaa6e7b42ef5e22a5
ragor114 <69634980+ragor114@users.noreply.github.com>
2020-09-12 16:12:54 +0200

    changed Encryption-Algorithm back

--- app/src/main/java/ur/mi/liebestagebuch/Encryption/EncryptionConfig.java
+++ app/src/main/java/ur/mi/liebestagebuch/Encryption/EncryptionConfig.java
@@ -12,7 +12,7 @@ public class EncryptionConfig {
     public final static String KEY_FACTORY_ALGORITHM = "PBKDF2WithHmacSHA256";
     public final static int SALT_SIZE = 8;
     public final static int INTERATRION_COUNT = 65536;
-    public final static int KEY_LENGTH = 128;
+    public final static int KEY_LENGTH = 256;
     public final static String CHARSET_NAME = "UTF-8";
 
 }
