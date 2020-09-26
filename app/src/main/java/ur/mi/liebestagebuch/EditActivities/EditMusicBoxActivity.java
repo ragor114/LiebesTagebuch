c76358038b9d263726193a574a0f9c54f3125792
ragor114 <69634980+ragor114@users.noreply.github.com>
2020-09-26 13:21:48 +0200

    added ugly layout for spotify-Edit-Acitivity but connection still not working

--- app/src/main/java/ur/mi/liebestagebuch/EditActivities/EditMusicBoxActivity.java
+++ app/src/main/java/ur/mi/liebestagebuch/EditActivities/EditMusicBoxActivity.java
@@ -10,15 +10,19 @@ import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
 
+import ur.mi.liebestagebuch.R;
+
 public class EditMusicBoxActivity extends AppCompatActivity {
 
     private static final String CLIENT_ID = "ce7c78a74aa7477989eae35bc8725657";
-    private static final String REDIRECT_URI = "ur.mi.liebestagebuch://callback";
+    private static final String REDIRECT_URI = "localhost://callback";
     private SpotifyAppRemote appRemote;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_music_box_activity);
+        Log.d("Spotify", "EditMusicBox created");
     }
 
     @Override
@@ -28,10 +32,12 @@ public class EditMusicBoxActivity extends AppCompatActivity {
                 .setRedirectUri(REDIRECT_URI)
                 .showAuthView(true)
                 .build();
+        Log.d("Spotify", "ConnectionParams built");
         SpotifyAppRemote.connect(this, connectionParams, new Connector.ConnectionListener() {
             @Override
             public void onConnected(SpotifyAppRemote spotifyAppRemote) {
                 appRemote = spotifyAppRemote;
+                appRemote.getPlayerApi().play("spotify:playlist:6uJdeXLzNtFPEhuZ0XFid0");
                 Log.d("Spotify", "App Remote connected!");
             }
 
@@ -40,5 +46,11 @@ public class EditMusicBoxActivity extends AppCompatActivity {
                 Log.d("Spotify", "App Remote could not connect: " + throwable.getMessage());
             }
         });
+        Log.d("Spotify", "Tried to connect");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
     }
 }
