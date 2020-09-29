c664df8cb904d8cc656d7fe78afebebf2aa84ae7
ragor114 <69634980+ragor114@users.noreply.github.com>
2020-09-29 12:35:04 +0200

    Fixed problem when trying to edit  Entry while last Entry isn't saved yet

--- app/src/main/java/ur/mi/liebestagebuch/GridView/GridActivity.java
+++ app/src/main/java/ur/mi/liebestagebuch/GridView/GridActivity.java
@@ -60,10 +60,14 @@ public class GridActivity extends AppCompatActivity implements AdapterView.OnIte
     private byte[] lastEditedSalt;
     private int lastEditedEntryEmotion;
 
+    private boolean savingLastEntryFinished;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.grid_activity);
+
+        savingLastEntryFinished = true;
 
         initGrid();
         dbHelper = new DBHelper(this, this);
@@ -143,13 +147,17 @@ public class GridActivity extends AppCompatActivity implements AdapterView.OnIte
     // dieses der DetailActivity als Extra übergeben, die forResult gestartet wird.
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Entry clickedEntry = entries.get(position);
-        Date clickedEntryDate = clickedEntry.getDate();
-        Emotion clickedEntryEmotion = clickedEntry.getEmotion();
+        if(savingLastEntryFinished) {
+            Entry clickedEntry = entries.get(position);
+            Date clickedEntryDate = clickedEntry.getDate();
+            Emotion clickedEntryEmotion = clickedEntry.getEmotion();
 
-        Intent intent = new Intent (GridActivity.this, DetailActivity.class);
-        intent.putExtra(DetailActivityConfig.ENTRY_DATE_KEY, DateUtil.setToMidnight(clickedEntryDate));
-        startActivityForResult(intent, DetailActivityConfig.START_DETAIL_ACTIVITY_REQUEST_CODE);
+            Intent intent = new Intent(GridActivity.this, DetailActivity.class);
+            intent.putExtra(DetailActivityConfig.ENTRY_DATE_KEY, DateUtil.setToMidnight(clickedEntryDate));
+            startActivityForResult(intent, DetailActivityConfig.START_DETAIL_ACTIVITY_REQUEST_CODE);
+        } else{
+            Toast.makeText(this, "Please wait while last Entry is saved, this can take a minute.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void requestAllEmotions(){
@@ -212,6 +220,7 @@ public class GridActivity extends AppCompatActivity implements AdapterView.OnIte
                 Log.d("Date", "After formattting milliseconds: " + entryDate.getTime());
                 lastEditedBoxListString = boxListString;
                 lastEditedEntryEmotion = emotionInt;
+                savingLastEntryFinished = false;
                 dbHelper.newEntry(entryDate, emotionInt, "", null, null);
             }
         }
@@ -286,6 +295,7 @@ public class GridActivity extends AppCompatActivity implements AdapterView.OnIte
             case DetailActivityConfig.EMOTION_UPDATE_CODE:
                 Log.d("Detail", "Entry update complete");
                 lastEditedEntryEmotion = 0;
+                savingLastEntryFinished = true;
                 break;
         }
     }
