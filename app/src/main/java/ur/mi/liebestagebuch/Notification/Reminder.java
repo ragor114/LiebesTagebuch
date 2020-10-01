d66062455877764d8637282b4939162b8bc04f4a
BrokeMo <69637383+BrokeMo@users.noreply.github.com>
2020-10-01 18:12:05 +0200

    Update Reminder.java

--- app/src/main/java/ur/mi/liebestagebuch/Notification/Reminder.java
+++ app/src/main/java/ur/mi/liebestagebuch/Notification/Reminder.java
@@ -1,7 +1,6 @@
 package ur.mi.liebestagebuch.Notification;
 
-import android.app.AlarmManager;
-import android.app.Notification;
+
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
@@ -10,50 +9,17 @@ import android.content.Intent;
 import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-
-import ur.mi.liebestagebuch.R;
 
 public class Reminder extends BroadcastReceiver{
 
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d("notification", "onrecieve");
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        Intent repeating_intent = new Intent(context, RepeatingActivity.class);
-        repeating_intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        NotificationHelper notificationHelper =new NotificationHelper(context);
+        NotificationCompat.Builder nb = notificationHelper.getChannelNotification();
+        notificationHelper.getManager().notify(1, nb.build());
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 100, repeating_intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "notification")
-                .setSmallIcon(R.drawable.android10logo)
-                .setContentIntent(pendingIntent)
-                //.setContentTitle(R.string.notification_title)
-                //.setContentText(R.string.notification_text)
-                .setContentTitle("Dear Diary...")
-                .setContentText("Don't forget to write a diary entry today!")
-                .setAutoCancel(false)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
-        notificationManager.notify(200, builder.build());
     }
-
-   /* public void setService (Context context) {
-        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        Intent i = new Intent(context, Reminder.class);
-        PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 60 * 60 *24, pi); // Millisekunden * Sekunden * Minuten
-    }
-
-    public boolean cancelService(Context context) {
-        Intent intent = new Intent(context, Reminder.class);
-        PendingIntent sender = PendingIntent.getBroadcast(context, 0, intent, 0);
-        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        alarmManager.cancel(sender);
-
-        return true;
-    }
-    */
 
 }
