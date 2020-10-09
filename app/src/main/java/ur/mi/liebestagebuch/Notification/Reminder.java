2e5260891ab139db5cefff93c646f03af60fdd48
BrokeMo <69637383+BrokeMo@users.noreply.github.com>
2020-10-09 17:08:19 +0200

    Update Reminder.java

--- app/src/main/java/ur/mi/liebestagebuch/Notification/Reminder.java
+++ app/src/main/java/ur/mi/liebestagebuch/Notification/Reminder.java
@@ -11,6 +11,7 @@ import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
 
+import ur.mi.liebestagebuch.LoginActivity;
 import ur.mi.liebestagebuch.Settings.SettingsConfig;
 
 public class Reminder extends BroadcastReceiver{
@@ -22,11 +23,17 @@ public class Reminder extends BroadcastReceiver{
         SharedPreferences sharedPreferences = context.getSharedPreferences(SettingsConfig.SHARED_PREFS, Context.MODE_PRIVATE);
         boolean remind = sharedPreferences.getBoolean(SettingsConfig.SWITCH_REMIND, false);
 
+        // Die if-Abfrage überprüft, ob die Einstellung zur Erinnerung aktiviert wurde und schickt dann
+        // zum ausgewählten Zeitpunkt die Notification an den User.
         if(remind){
             Log.d("notification", "Reminding ...");
             NotificationHelper notificationHelper =new NotificationHelper(context);
             NotificationCompat.Builder nb = notificationHelper.getChannelNotification();
+            PendingIntent contentIntent = PendingIntent.getActivity(context, 0, new Intent(context, LoginActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+            nb.setContentIntent(contentIntent);
+            notificationHelper.getManager();
             notificationHelper.getManager().notify(1, nb.build());
+
         } else{
             Log.d("notification", "Reminder deactivated");
         }
